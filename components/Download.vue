af0b5cc414de85f3d1e5e2f64312bc840ca9cbec
Sam Garg <samgarg86@gmail.com>
2023-03-20 20:43:43 +0000

    download section adjust

--- components/Download.vue
+++ components/Download.vue
@@ -3,7 +3,7 @@
   <div class="py-12 bg-zinc-50">
     <div class="container">
       <div class="flex flex-wrap items-center justify-between">
-        <div class="xl:w-full md:w-3/5 xl:max-w-[33.8rem] lg:px-10 mb-4 md:mb-0">
+        <div class="mb-4 md:mb-0 md:w-3/5 xl:w-full xl:max-w-[28rem]">
           <h2 class="text-xl text-grey font-primary font-semibold tracking-tight leading-tight mb-9">Download Airalo app to manage and top up your eSIMs anytime, anywhere.</h2>
 
           <a href="#"><img class="inline-block mr-2 md:mr-5" src="~/assets/icons/AppleAppStore.svg" alt=""></a>
