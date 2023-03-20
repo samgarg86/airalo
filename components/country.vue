a386c781e58dc05c354d2be2f6b1cd1b4448cd22
Sam Garg <s.garg@astoundcommerce.com>
2023-03-20 12:17:47 +0000

    Scroll into view

--- components/country.vue
+++ components/country.vue
@@ -1,13 +1,11 @@
 <template>
   <div class="mb-3">
-    <div>
-      <button
-          class="flex w-full items-center bg-white rounded-lg shadow-lg p-4 relative after:content[''] after:w-[1.375rem] after:h-[1.375rem] after:inline-block after:absolute after:top-50 after:right-4 after:bg-angle-up after:bg-no-repeat after:bg-center"
-          @click="clicked">
-        <span><img class="w-10 mr-4" :src="image.url" alt=""></span>
-        <span class="font-primary font-semibold text-base text-grey">{{title}}</span>
-      </button>
-    </div>
+    <button
+        class="flex w-full items-center bg-white rounded-lg shadow-lg p-4 relative after:content[''] after:w-[1.375rem] after:h-[1.375rem] after:inline-block after:absolute after:top-50 after:right-4 after:bg-angle-up after:bg-no-repeat after:bg-center"
+        @click="clicked" ref="buttonRef">
+      <span><img class="w-10 mr-4" :src="image.url" alt=""></span>
+      <span class="font-primary font-semibold text-base text-grey">{{title}}</span>
+    </button>
     <div class="block" v-show="active">
       <div class="pt-9 flex flex-wrap -mx-4">
         <Plan v-for="p in packages" :key="p.id" v-bind="p" :country="title"/>
@@ -23,7 +21,7 @@ const props = defineProps({
   active: Boolean
 })
 const packages = ref([])
-// const active = ref(false)
+const buttonRef = ref(null)
 const emit = defineEmits(['clicked'])
 const clicked = async () => {
   if (!packages.value?.length) {
@@ -31,6 +29,7 @@ const clicked = async () => {
     packages.value = data.value.packages
   }
   emit('clicked')
+  buttonRef.value?.scrollIntoView()
 }
 
 </script>
