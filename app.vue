4c2a83c0cd91f78d634a75aab9d3af63f019f308
Sam Garg <samgarg86@gmail.com>
2023-03-22 13:32:53 +0000

    description

--- app.vue
+++ app.vue
@@ -15,12 +15,12 @@ useHead({
   meta: [
     { charset: 'utf-8' },
     { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-    { hid: 'description', name: 'description', content: 'description' },
+    { hid: 'description', name: 'description', content: 'Airalo Website Engineering Task' },
     { name: 'format-detection', content: 'telephone=no' },
     { 'http-equiv': 'accept-ch', 'content': 'width' }
   ],
   link: [
-      { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+    { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
     { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
     { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@100;200;300;400;500;600;700&display=swap' },
   ]
